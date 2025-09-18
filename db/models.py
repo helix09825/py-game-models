6ad4c9e845b5691ae54f49e1384cefb98bd4e829
helix09825 <maksilczuk@gmail.com>
2025-09-19 00:01:35 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Race(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255, blank=True, null=True)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skills")
 
 
 class Guild(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
 
@@ -21,7 +23,11 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=False)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL,
-                              null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="players")
+    guild = models.ForeignKey(Guild,
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              blank=True,
+                              related_name="members")
+    created_at = models.DateTimeField(default=timezone.now)
--- main.py
+++ main.py
@@ -1,39 +1,45 @@
-import init_django_orm  # noqa: F401
 import json
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
     with open("players.json", "r") as file:
         players_data = json.load(file)
-        for nickname, player_data in players_data.items():
-            race, created = Race.objects.get_or_create(
-                name=player_data["race"]["name"],
-                defaults={"description": player_data["race"]["description"]}
+    for nickname, player_data in players_data.items():
+        race_data = player_data.get("race", {})
+        guild_data = player_data.get("guild")
+        email = player_data.get("email")
+        bio = player_data.get("bio")
+
+        race, created = Race.objects.get_or_create(
+            name=race_data.get("name"),
+            defaults={"description": race_data.get("description", "")}
+        )
+
+        skills = race_data.get("skills", [])
+        for skill_data in skills:
+            Skill.objects.get_or_create(
+                name=skill_data.get("name"),
+                defaults={"bonus": skill_data.get("bonus", ""), "race": race}
             )
-            for skill_data in player_data["race"]["skills"]:
-                if player_data["race"]["skills"]:
-                    skill, created = Skill.objects.get_or_create(
-                        name=skill_data["name"],
-                        race=race,
-                        defaults={"bonus": skill_data["bonus"]}
-                    )
-            guild = None
-            if player_data["guild"] is not None:
-                guild, created = Guild.objects.get_or_create(
-                    name=player_data["guild"]["name"],
-                    defaults={
-                        "description": player_data["guild"]["description"]}
-                )
-            player, created = Player.objects.get_or_create(
-                nickname=nickname,  # nickname to klucz z pętli
-                defaults={
-                    "email": player_data["email"],
-                    "bio": player_data["bio"],
-                    "race": race,
-                    "guild": guild  # może być None
-                }
+
+        guild = None
+        if guild_data is not None:
+            guild, created = Guild.objects.get_or_create(
+                name=guild_data.get("name"),
+                defaults={"description": guild_data.get("description")}
             )
+
+        Player.objects.get_or_create(
+            nickname=nickname,
+            defaults={
+                "email": email,
+                "bio": bio,
+                "race": race,
+                "guild": guild
+            }
+        )
 
 
 if __name__ == "__main__":
